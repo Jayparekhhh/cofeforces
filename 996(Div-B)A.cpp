94a5bf8f36eab0499b727381197a672720ec194f
Parekh Jay Darshanbhai <100278532+Jayparekhhh@users.noreply.github.com>
2025-01-13 20:12:43 +0530

    Update 996(Div-B)A.cpp

--- 996(Div-B)A.cpp
+++ 996(Div-B)A.cpp
@@ -1,1 +1,20 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin>>t;
+for(int i = 0; i<t; i++){
+    int n; int a; int b;
+    cin>>n>>a>>b;
+    if(abs(a-b)%2 == 0){
+        cout<<"YES"<<endl;
+     }
+    else{
+        cout<<"NO"<<endl;
+    }
+    }
+    return 0;
+    }
